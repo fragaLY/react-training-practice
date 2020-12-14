4258c860aab595447881f754adcd8492ee0942a8
fragaly <vadzim.kavalkou@gmail.com>
2020-12-14 22:08:05 +0300

    Custom menu

---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import classes from './Backdrop.module.scss'
+
+const Backdrop = props => <div className={classes.Backdrop} onClick={props.onClick}></div>
+
+export default Backdrop
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import classes from './Layout.module.scss'
 import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle";
+import Drawer from "../../components/Navigation/Drawer/Drawer";
 
 class Layout extends Component {
     state = {
@@ -13,9 +14,19 @@ class Layout extends Component {
         })
     }
 
+    menuCloseHandler = () => {
+        this.setState({
+            menu: false
+        })
+    }
+
     render() {
         return (
             <div className={classes.Layout}>
+                <Drawer
+                    isOpen={this.state.menu}
+                    onClose={this.menuCloseHandler}
+                />
                 <MenuToggle
                     onToggle={this.toggleMenuHandler}
                     isOpen={this.state.menu}
